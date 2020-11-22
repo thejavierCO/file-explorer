ad286bc869269da42161b5d130f3b414a087d195
javier cruz <javier.cruz.odispo@hotmail.com>
2020-11-21 21:29:11 -0600

    start

--- src/tools/dir.js
+++ src/tools/dir.js
@@ -5,24 +5,24 @@ class Dir{
     constructor(root,manager){
         this.type = "dir";
         this.set = (name)=>!fs.existsSync(path.resolve(root,name))?
-        fs.mkdirSync(path.resolve(root,name))
-        :((a)=>{
-            throw {error:"exist directory",path:a}
-        })(path.resolve(root,name));
+            fs.mkdirSync(path.resolve(root,name))
+            :((a)=>{
+                throw {error:"exist directory",path:a}
+            })(path.resolve(root,name));
         this.get = (name)=>fs.existsSync(path.resolve(root,name))?
-        new manager(path.resolve(root,name)):
-        ((a)=>{
-            throw {error:"not exist directory",path:a}
-        })(path.resolve(root,name))
+            new manager(path.resolve(root,name)):
+            ((a)=>{
+                throw {error:"not exist directory",path:a}
+            })(path.resolve(root,name))
         this.del = (name)=>name?fs.existsSync(path.resolve(root,name))?
-        fs.rmdirSync(path.resolve(root,name)):
-        ((a)=>{
-            throw {error:"not exist directory",path:a}
-        })(path.resolve(root,name)):fs.existsSync(path.resolve(root))?
-        fs.rmdirSync(path.resolve(root)):
-        ((a)=>{
-            throw {error:"not exist directory",path:a}
-        })(path.resolve(root))
+            fs.rmdirSync(path.resolve(root,name)):
+            ((a)=>{
+                throw {error:"not exist directory",path:a}
+            })(path.resolve(root,name)):fs.existsSync(path.resolve(root))?
+            fs.rmdirSync(path.resolve(root)):
+            ((a)=>{
+                throw {error:"not exist directory",path:a}
+            })(path.resolve(root))
         this.read = (name)=>{}
         this.add = (element)=>{}
         this.filter = (fs)=>{}
