10513a2137218de1874c4ee9d2bfe9620a5d404e
javier cruz <javier.cruz.odispo@hotmail.com>
2020-11-25 15:46:14 -0600

    clear in run 0

--- src/tools/create/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Icreate } from "../../Types/elements";
-declare class create implements Icreate {
-    constructor(root: string);
-}
-export default create;
-//# sourceMappingURL=index.d.ts.map
